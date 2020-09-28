f1e237ee8d96585a419cbe8e0c2ce7b1fb18500f
Marco Altamirano <maaltamiranohiga@gmail.com>
2020-09-28 17:42:25 -0500

    falta arreglar el rango de valores para q no explote

--- js/script.js
+++ js/script.js
@@ -1,74 +1,135 @@
+alert("Los valores están truncados a 2 decimales, para ver los valores originales revise la consola")
 function creaMatriz() {
 	var maximo = 100; //Máximo de rango
 	var minimo = 1; //Minimo de rango
 	var t = document.getElementById("numero_filas_columnas").value;
-	console.log(t)
+	var dete = -1;
 	if (t < 4 || t > 10){
 		alert("El valor ingresado no es valido (Mayor a 10 o Menor a 4)");
-	} else{
-		var arreglo = new Array((t*(t+1))/2);
-		for (var i = 0; i < (t*(t+1))/2; ++i){
-			arreglo[i] = Math.floor((Math.random() * (maximo - minimo + 1)) +  minimo);
-		}
-		var matriz = [];
-		for (var i = 0; i < t; ++i){
-			matriz[i] = new Array(t);
-		}
-		var contador = 0;
-		for (var i = 0; i < t; ++i){
-			for (var j = i; j < t; ++j){
-				matriz[i][j] = arreglo[contador];
-				matriz[j][i] = arreglo[contador];
-				contador++;
+	}
+	else{
+		while (dete < 0){
+			var arreglo = new Array((t*(parseInt(t)+1))/2);
+			for (var i = 0; i < (t*(parseInt(t)+1))/2; ++i){
+				arreglo[i] = Math.floor((Math.random() * (maximo - minimo + 1)) +  minimo);
 			}
+			var matriz = [];
+			for (var i = 0; i < t; ++i){
+				matriz[i] = new Array(t);
+			}
+			var contador = 0;
+			for (var i = 0; i < t; ++i){
+				for (var j = i; j < t; ++j){
+					matriz[i][j] = arreglo[contador];
+					matriz[j][i] = arreglo[contador];
+					contador++;
+				}
+			}
+			dete = determinante(matriz);
 		}
-		console.log(matriz);
-		Cholesky(matriz, t);
+		if (!Cholesky(matriz, t)) creaMatriz();
 	}
 }
+function createMatriz(t){
+	var matriz = new Array(t);
+	for (i = 0; i < t ; i++){ 
+		matriz[i]=new Array(t); 
+	}
+	return matriz;
+}
+function determinante(matriz){
+	if(matriz.length==2){
+		var det=(matriz[0][0]*matriz[1][1])-(matriz[1][0]*matriz[0][1]);
+		return det;
+	}               
+	var suma = 0;
+	for(var i = 0; i<matriz.length; i++){
+		var nm = createMatriz(matriz.length-1);
+		for(var j=0; j<matriz.length; j++){
+			if(j!=i){
+				for(var k=1; k<matriz.length; k++){
+					var indice=-1;
+					if(j<i) indice=j;
+			        else if(j>i) indice=j-1;
+			        nm[indice][k-1] = matriz[j][k];
+			    }
+			}
+		}
+		if(i%2==0){                            
+			suma += matriz[i][0] * determinante(nm);                            
+		}                            
+		else{                            
+			suma -= matriz[i][0] * determinante(nm);
+		}                        
+	}
+	return suma;
+}
 function Cholesky(matriz, n){
-	let lower = [];
-	let definida = true;
-	for (i = 0; i < n; ++i){
+var lower = [];
+var definida = true;
+
+	for (var i = 0; i < n; ++i){
 		lower[i] = new Array(n);
 	}
-	for (i = 0; i < n; ++i){
-		for (j = 0; j < n; ++j){
-			let sum = 0;
-			if (i == j){
-				for (k = 0; k < j; ++k){
+
+	for (var i = 0; i < n; ++i){
+		for (var j = 0; j < n; ++j){
+			var sum = 0;
+			if (j == i){
+				for (var k = 0; k < j; ++k){
 					sum += Math.pow(lower[j][k], 2);
 				}
 				if (matriz[j][j] - sum < 0){
-					alert("Matriz no definida positivamente");
+					//alert("Matriz no definida positvamente");
 					definida = false;
-				} else {
-					lower[j][j] = Math.sqrt(matriz[j][j] - sum);
 				}
+				else lower[j][j] = Math.sqrt(matriz[j][j] - sum);
 			} else{
-				for (k = 0; k < j; ++k){
+				for (var k = 0; k < j; ++k){
 					sum += (lower[i][k] * lower[j][k]);
 				}
-				lower[i][j] = (matriz[i][j] - sum) / lower[j][j];
+					lower[i][j] = (matriz[i][j] - sum) / lower[j][j];
 			}
 		}
 	}
+
 	if (definida){
-		for (i = 0; i < n; ++i){
-			for(j = 0; j < n; ++j){
+
+		for (var i = 0; i < n; ++i){
+			for(var j = 0; j < n; ++j){
 				if (isNaN(lower[i][j])) lower[i][j] = 0;
 			}
 		}
-		return lower
+		imprimir_matriz(matriz);
+		let box_matriz = document.getElementById("caja_matriz");
+		let caja_igual = document.createElement("div")
+		let caja_multiplicacion = document.createElement("div")
+		caja_igual.innerHTML += "="
+		caja_igual.classList.add("caja_igual")
+		box_matriz.append(caja_igual)
+		imprimir_matriz(lower)
+		caja_multiplicacion.innerHTML += "x"
+		caja_multiplicacion.classList.add("caja_igual")
+		box_matriz.append(caja_multiplicacion)
+		imprimir_matriz(transpose(lower))
+		return true;
 	} else{
-		alert("La matriz no es definida positvamente");
+		//console.log("La matriz no es definida positvamente");
+		return false;
 	}
 }
+function dosDecimales(n) {
+  let t=n.toString();
+  let regex=/(\d*.\d{0,2})/;
+  return t.match(regex)[0];
+}
 function imprimir_matriz(mat){
+	console.log(mat)
 	let box_matriz = document.getElementById("caja_matriz");
 	for(i = 0;i<mat.length;i++){
 		let div_row = document.createElement("div");
 		for(j = 0;j<mat[i].length;j++){
+			mat[i][j] = dosDecimales(mat[i][j])
 			let new_div = document.createElement("div");
 			if(i == 0){
 				new_div.classList.add("border_left");
@@ -82,21 +143,7 @@ function imprimir_matriz(mat){
 	}
 	
 }
-function resolver(mat){
-	imprimir_matriz(mat)
-	let box_matriz = document.getElementById("caja_matriz");
-	let matriz_res = Cholesky(mat,mat.length)
-	let caja_igual = document.createElement("div")
-	let caja_multiplicacion = document.createElement("div")
-	caja_igual.innerHTML += "="
-	caja_igual.classList.add("caja_igual")
-	box_matriz.append(caja_igual)
-	imprimir_matriz(matriz_res)
-	caja_multiplicacion.innerHTML += "x"
-	caja_multiplicacion.classList.add("caja_igual")
-	box_matriz.append(caja_multiplicacion)
-	imprimir_matriz(transpose(matriz_res))
-}
+
 function pruebas(){
 	mat = [
 	[4, 12, -16],
